45c9c1629b4eaeb65293008334bb2ee7ea1b4ab9
KharlAbban <khvngkharl123@gmail.com>
2024-07-12 11:28:33 +0000

    Initialize database and start fetching courts

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,9 +4,12 @@ const express = require('express');
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const courtRouter = require("./routes/CourtRoutes");
 
 const app = express();
 const port_no = process.env.PORT_NO;
+const MONGODB_URI = process.env.MONGO_URI_LOCAL;
 
 app.use(cors());
 app.use(cookieParser());
@@ -14,10 +17,27 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: true}));
 
-app.listen(port_no, (req, res) => {
-    console.log(`Server running on port ${port_no}`);
-});
+app.use("/courts", courtRouter);
 
-app.get("/", (req, res) => {
-    res.send("Hi");
-})
+const startServer = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+
+        app.listen(port_no, (req, res) => {
+            console.log(`Server running on port ${port_no}`);
+        });
+
+        app.get("/", (req, res) => {
+            res.send("Hi");
+        });
+
+        app.get("*", (req, res) => {
+            res.status(404).send("No route found!");
+        });
+        
+    } catch (err) {
+        console.error("Failed to connect to Mongo", err.message);
+    }
+}
+
+startServer();
